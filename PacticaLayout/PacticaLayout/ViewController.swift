1b8b285f661bade998a2b2fb6342561303d23935
rech99 <53828060+rech99@users.noreply.github.com>
2021-09-07 17:32:23 -0700

    Add files via upload

---
+++ PacticaLayout/PacticaLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PacticaLayout
+//
+//  Created by Alumno on 9/7/21.
+//  Copyright © 2021 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
